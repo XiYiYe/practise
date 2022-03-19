85855e487aeb9f6f6d5ebd6a9d680296f8caa440
yexuehui-jk <yexuehui-jk@360shuke.com>
2022-03-19 21:44:50 +0800

    ddd项目结构

---
+++ ddd/facade/src/test/java/com/example/facade/FacadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.facade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacadeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ ddd/infrastructure/src/main/java/com/example/infrastructure/InfrastructureApplication.java
@@ -0,0 +1,13 @@
+package com.example.infrastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfrastructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfrastructureApplication.class, args);
+    }
+
+}
